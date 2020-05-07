773e4f4fa68e925f044e29219825a1f66e33d517
larmas7 <lucas.armas.7@gmail.com>
2020-05-07 13:14:49 -0300

    Basic structure

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const cons= require('consolidate');
+const routes = require('./javascript/routes');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+app.use('/', routes);
+
+app.set('port', 8080);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.engine('html', cons.swig)
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'html');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.listen(app.get('port'), () => {
+ console.log('Express started on http://localhost:' + app.get('port') + ' press Ctrl-C to terminate');
+});
+
+module.exports = app;
